6f9071657033e1f6befa39fe0732b65da2a7d57c
rajrishivishwakarma <whowantsthisemailid@gmail.com>
2025-03-27 17:11:23 +0530

    init

---
+++ src/main/java/com/example/springlearnings/SpringlearningsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlearnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlearningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlearningsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springlearnings/SpringlearningsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlearnings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlearningsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
