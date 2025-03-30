f92c2c5f5702fa3edbf07bf416137d6c2f75bba1
rajrishivishwakarma <whowantsthisemailid@gmail.com>
2025-03-30 16:27:30 +0530

    user service layer and exception is ok

--- src/main/java/com/example/springlearnings/services/errorhandling/exceptions/UserAlreadyExistException.java
+++ src/main/java/com/example/springlearnings/services/errorhandling/exceptions/UserAlreadyExistException.java
@@ -1,7 +1,7 @@
 package com.example.springlearnings.services.errorhandling.exceptions;
 
-public class UserAlreadyExistsException extends Exception{
-    public UserAlreadyExistsException(){
+public class UserAlreadyExistException extends Exception{
+    public UserAlreadyExistException(){
         super("Username already exists!");
     }
 }
---
+++ src/main/java/com/example/springlearnings/services/interfaces/IUserManagementService.java
@@ -0,0 +1,15 @@
+package com.example.springlearnings.services.interfaces;
+
+import com.example.springlearnings.entity.User;
+import com.example.springlearnings.services.errorhandling.exceptions.UserAlreadyExistException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface IUserManagementService {
+    List<User> getAllUsers();
+    User getUserByUserName(String username);
+    void createUser(User user) throws UserAlreadyExistException;
+    void deleteUser(String username);
+}
