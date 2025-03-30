ee8b08abda11c6f871c67ac2da718c4bd5849a2a
rajrishivishwakarma <whowantsthisemailid@gmail.com>
2025-03-30 16:31:08 +0530

    user service impl

---
+++ src/main/java/com/example/springlearnings/services/impl/UserManagementService.java
@@ -0,0 +1,39 @@
+package com.example.springlearnings.services.impl;
+
+import com.example.springlearnings.entity.User;
+import com.example.springlearnings.persistence.IUserRepository;
+import com.example.springlearnings.services.errorhandling.exceptions.UserAlreadyExistException;
+import com.example.springlearnings.services.interfaces.IUserManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserManagementService implements IUserManagementService {
+    @Autowired
+    private IUserRepository repository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
+
+    @Override
+    public User getUserByUserName(String username) {
+        return repository.findByUsername(username);
+    }
+
+    @Override
+    public void createUser(User user) throws UserAlreadyExistException {
+        if(repository.existsById(user.getId()))
+            throw new UserAlreadyExistException();
+        else
+            repository.save(user);
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        repository.deleteByUsername(username);
+    }
+}
